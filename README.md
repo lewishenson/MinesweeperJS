90dee5a015cc1e319a725b6df42978915436c6cc
Lewis Henson <lewishenson@users.noreply.github.com>
2014-05-10 14:27:08 +0100

    Add mines to minefield.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ AngularJS implementation of the Minesweeper game, heavily based on the blog post
 1. [Part I](http://www.simplygoodcode.com/2014/04/angularjs-game-programming-making-minesweeper/) - introduction to Minesweeper
 2. [Part II](http://www.simplygoodcode.com/2014/04/angularjs-game-programming-making-minesweeper-part-ii/) - basic grid
 3. [Part III](http://www.simplygoodcode.com/2014/04/angularjs-game-programming-making-minesweeper-part-iii/) - uncovering squares
+4. [Part IV](http://www.simplygoodcode.com/2014/05/angularjs-game-programming-making-minesweeper-part-iv/) - adding mines
--- minefield-controller.js
+++ minefield-controller.js
@@ -1,26 +1,60 @@
 function MinesweeperController($scope) {
-    function createMinefield() {
-        var minefield = {
-            rows: []
+    $scope.minefield = this._createMinefield(9, 9, 10);
+}
+
+MinesweeperController.prototype._createMinefield = function (numberOfRows, numberOfColumns, numberOfMines) {
+    var minefield = {
+        rows: []
+    };
+
+    for (var i = 0; i < numberOfRows; i++) {
+        var row = {
+            squares: []
         };
 
-        for (var i = 0; i < 9; i++) {
-            var row = {
-                squares: []
+        for (var j = 0; j < numberOfColumns; j++) {
+            var square = {
+                isCovered: true,
+                content: 'empty'
             };
-
-            for (var j = 0; j < 9; j++) {
-                var square = {
-                    isCovered: true
-                };
-                row.squares.push(square);
-            }
-
-            minefield.rows.push(row);
+            row.squares.push(square);
         }
 
-        return minefield;
+        minefield.rows.push(row);
     }
 
-    $scope.minefield = createMinefield();
-}
+    var getSquare = function (rowIndex, columnIndex) {
+        return minefield.rows[rowIndex].squares[columnIndex];
+    };
+
+    var placeRandomMine = function () {
+        var rowIndex = Math.floor(Math.random() * numberOfRows);
+        var columnIndex = Math.floor(Math.random() * numberOfColumns);
+        var square = getSquare(rowIndex, columnIndex);
+        square.content = 'mine';
+    };
+
+    for (var i = 0; i < numberOfMines; i++) {
+        placeRandomMine();
+    }
+
+    while (this._getNumberOfMines(minefield) < numberOfMines) {
+        placeRandomMine();
+    }
+
+    return minefield;
+};
+
+MinesweeperController.prototype._getNumberOfMines = function (minefield) {
+    var count = 0;
+
+    _.forEach(minefield.rows, function (row) {
+        _.forEach(row.squares, function (square) {
+            if (square.content === 'mine') {
+                count++;
+            }
+        });
+    });
+
+    return count;
+};
